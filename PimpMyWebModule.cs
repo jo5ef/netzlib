efde47a4adc218375ce77e4561c62bf6118b556c
Josef Pfleger <green-carrot@orange-cactus.org>
2010-03-11 21:54:14 +0100

    Added Module

---
+++ PimpMyWebModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using PimpMyWeb.Javascript;
+
+namespace PimpMyWeb
+{
+	public class PimpMyWebModule : IHttpModule
+	{
+		public void Dispose()
+		{
+		}
+
+		public void Init(HttpApplication context)
+		{
+			context.BeginRequest += new EventHandler(context_BeginRequest);
+		}
+
+		void context_BeginRequest(object sender, EventArgs e)
+		{
+			HttpContext.Current.Response.Filter = new ScriptTagFilter();
+		}
+	}
+}
